e488e1da5e38852cfe23c7c4911e2da941174e55
Keith <user@Keiths-Macbook-Pro.local>
2013-07-12 13:50:30 -0700

    Initial Commit

---
+++ PrimerProjects/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PrimerProjects
+//
+//  Created by Keith on 7/12/13.
+//  Copyright (c) 2013 Appology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
